c9cb9ed6405468b62b53e513686a95391e9584e9
Leonardo Abreu <leonardo.abreu@ad.ufrgs.br>
2019-05-10 14:09:39 -0300

    Criado Smoke, Gneric e Searchs testes

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,50 +1,156 @@
-// eslint-disable-next-line prefer-destructuring
-const expect = require('chai').expect;
-// eslint-disable-next-line no-undef
-describe('Main', () => {
-  let arr;
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
-  // Roda uma vez antes do bloco
-  // eslint-disable-next-line no-undef
-  before(() => {
-  });
+global.fetch = require('node-fetch')
 
-  // Roda uma vez depois do bloco
-  // eslint-disable-next-line no-undef
-  after(() => {
-  });
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main'
 
-  // Roda todas as vezes antes de um bloco
-  // eslint-disable-next-line no-undef
+describe('Spotify Wrapper', () => {
+  
+  let fetchedStub;
+  let promise;
+
   beforeEach(() => {
-    arr = [1, 2, 3];
+    fetchedStub = sinon.stub(global, 'fetch');
+    promise = fetchedStub.returnsPromise();
   });
 
-  // Roda todas as vezes depois de um bloco
-  // eslint-disable-next-line no-undef
   afterEach(() => {
+    fetchedStub.restore();
   });
 
-  // eslint-disable-next-line no-undef
-  it('should be an array', () => {
-    expect(arr).to.be.a('array');
+  // Smoke Testes => Garantir que uma função exista.
+  describe('Smoke Tests:', () => {
+    
+
+    //search(genérico)
+    //searchAlbums
+    //searchArtists
+    //searchTracks
+    //searchPlaylists
+
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+
+
   });
 
-  // eslint-disable-next-line no-undef
-  it('should have a size of 4 when push another value to the array', () => {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4);
+  describe('Generic Search', () => {
+
+    it('should call fetch function', () => {
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should recive the correct URL to fetch', () => {
+
+      context('passing one type', () => {
+
+        const artists = search('Incubus', 'artist');      
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+        const albums = search('Incubus', 'albums');      
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=albums');
+
+      });
+
+      context('passing more than one type', () => {
+
+        const artistsAndAlbums = search('Incubus', ['album', 'artist']);
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album,artist')
+
+      });
+      
+    });
+
+    it('should return the JSON Data from the Promise', () => {
+      promise.resolves({body: 'json'});
+      const artists = search('Incubus', 'artist');
+
+      expect(artists.resolveValue).to.be.eql({body: 'json'});
+    });
+
   });
 
-  // eslint-disable-next-line no-undef
-  it('should have a size of 2 when pop a value from the array', () => {
-    arr.pop(4);
-    expect(arr).to.have.lengthOf(2);
+  describe('Seach Artists', () => {
+    
+    it('should call fetch function', () => {
+      const artists = searchArtists('Incubus');
+      expect(fetchedStub).to.have.been.calledOnce;     
+    });
+
+    it('should call fetch with correct URL', () => {
+      const artists = searchArtists('Incubus');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist'); 
+    });
+
   });
 
-  // eslint-disable-next-line no-undef
-  it('should remove 3 when use pop in the array', () => {
-    arr.pop();
-    expect(arr).to.not.include(3);
+  describe('Seach Albums', () => {
+    
+    it('should call fetch function', () => {
+      const albums = searchAlbums('Incubus');
+      expect(fetchedStub).to.have.been.calledOnce;     
+    });
+
+    it('should call fetch with correct URL', () => {
+      const albums = searchAlbums('Incubus');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album'); 
+    });
+
   });
+
+  describe('Seach Tracks', () => {
+    
+    it('should call fetch function', () => {
+      const tracks = searchTracks('Incubus');
+      expect(fetchedStub).to.have.been.calledOnce;     
+    });
+
+    it('should call fetch with correct URL', () => {
+      const tracks = searchTracks('Incubus');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=track'); 
+    });
+
+  });
+
+  describe('Seach Playlists', () => {
+    
+    it('should call fetch function', () => {
+      const playlists = searchPlaylists('Incubus');
+      expect(fetchedStub).to.have.been.calledOnce;     
+    });
+
+    it('should call fetch with correct URL', () => {
+      const playlists = searchPlaylists('Incubus');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=playlist'); 
+    });
+
+  });
+
 });
